4370b6abaa06da8be420b13a18ffada85f6f39cf
aleksandr <aleksandr.semenovsky@gmail.com>
2022-11-07 19:24:48 +0800

    it shows images!

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Post from './components/Post';
+
+const BASE_URL = 'http://localhost:8000/'
+
+function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+      .then(response => {
+        const json = response.json()
+        console.log(json)
+        if (response.ok) {
+          return json
+        }
+        throw response
+  })
+      .then(data => {
+        setPosts(data)
+      })
+      .catch(error => {
+        console.log(error)
+        alert(error)
+      })
+    }, [])
+
+  return (
+    <div className="app_posts">
+      {
+        posts.map(post => (
+          <Post post={post}/>)
+      )} 
+    </div>
+  );
+}
+
+export default App;
